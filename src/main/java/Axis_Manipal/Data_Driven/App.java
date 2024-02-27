6349c23a54b34e6a1fa47edf2fcff866d4b18326
Yashraj Sonawane <yashrajsonawane79@gmail.com>
2024-02-27 15:10:20 +0530

    First Axis Commit

---
+++ src/main/java/Axis_Manipal/Data_Driven/App.java
@@ -0,0 +1,13 @@
+package Axis_Manipal.Data_Driven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Axis_Manipal/Data_Driven/WriteDataToExcel.java
@@ -0,0 +1,98 @@
+package Axis_Manipal.Data_Driven;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WriteDataToExcel {
+
+WebDriver driver;
+	
+	XSSFWorkbook workbook;
+	
+	XSSFSheet sheet;
+	
+	XSSFCell cell;
+	@Test
+	public void fblogin() throws IOException
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.MILLISECONDS);
+		
+		//Import Excel sheet
+		
+		File src = new File("C:\\Users\\Lenovo\\eclipse-workspace\\Data_Driven\\TestData.xlsx");
+		
+		//Load the file
+		
+		FileInputStream fis = new FileInputStream(src);
+		
+		//load the workbook
+		
+		workbook = new XSSFWorkbook(fis);
+		
+		//access the sheet of the workbook
+		 
+		sheet = workbook.getSheetAt(0);
+		
+		for (int i=1 ; i<= sheet.getLastRowNum(); i++)
+		{
+			//import the data for email
+			
+			cell = sheet.getRow(i).getCell(0);
+			
+			driver.findElement(By.xpath("//input[@name='email']")).clear();
+			driver.findElement(By.xpath("//input[@name='email']")).sendKeys(cell.getStringCellValue());
+			
+			//import the data for password
+			
+			cell = sheet.getRow(i).getCell(1);
+			driver.findElement(By.xpath("//input[@id='pass']")).clear();
+			driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(cell.getStringCellValue());
+			
+			String title = driver.getTitle();
+			System.out.println(title);
+			
+			
+			//To write the Data in Excel sheet
+			
+			FileOutputStream fos = new FileOutputStream(src);
+			
+			//create the cell where the data need to be written
+			sheet.getRow(i).createCell(2).setCellValue(title);
+			
+			//Perform the write operation
+			workbook.write(fos);
+			
+			fos.close();			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
